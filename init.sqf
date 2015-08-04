A3Wasteland/ArmA3_Wasteland.Altis

Prevent third-party scripts from running on headless client

init.sqf
@@ -71,8 +71,11 @@ if (isServer) then
 	[] execVM "server\init.sqf";
 };
 
-//init 3rd Party Scripts
-[] execVM "addons\R3F_ARTY_AND_LOG\init.sqf";
-[] execVM "addons\proving_ground\init.sqf";
-[] execVM "addons\scripts\DynamicWeatherEffects.sqf";
-[] execVM "addons\JumpMF\init.sqf";
+if (hasInterface || isServer) then
+{
+	//init 3rd Party Scripts
+	[] execVM "addons\R3F_ARTY_AND_LOG\init.sqf";
+	[] execVM "addons\proving_ground\init.sqf";
+	[] execVM "addons\scripts\DynamicWeatherEffects.sqf";
+	[] execVM "addons\JumpMF\init.sqf";
+};

macchky/MAC_A3Wasteland.Tanoa

Conflicts:
	init.sqf

init.sqf
@@ -71,15 +71,17 @@ if (isServer) then
 	[] execVM "server\init.sqf";
 };
 
-//init 3rd Party Scripts
-[] execVM "addons\R3F_LOG\init.sqf";
-[] execVM "addons\proving_ground\init.sqf";
-[] execVM "addons\scripts\DynamicWeatherEffects.sqf";
-[] execVM "addons\zlt_fastrope\zlt_fastrope.sqf";
-[] execVM "addons\JumpMF\init.sqf";
-[] execVM "addons\EtV\init.sqf";
-[] execVM "addons\laptop\init.sqf";						// Addon for hack laptop mission
-[] execVM "addons\vactions\functions.sqf";				// Micovery vehicle actions
-[] execVM "addons\APOC_Airdrop_Assistance\init.sqf";	// Airdrop
-[] execVM "addons\AF_Keypad\AF_KP_vars.sqf";			// Keypad for base locking
-
+if (hasInterface || isServer) then
+{
+	//init 3rd Party Scripts
+	[] execVM "addons\R3F_LOG\init.sqf";
+	[] execVM "addons\proving_ground\init.sqf";
+	[] execVM "addons\scripts\DynamicWeatherEffects.sqf";
+	[] execVM "addons\zlt_fastrope\zlt_fastrope.sqf";
+	[] execVM "addons\JumpMF\init.sqf";
+	[] execVM "addons\EtV\init.sqf";
+	[] execVM "addons\laptop\init.sqf";						// Addon for hack laptop mission
+	[] execVM "addons\vactions\functions.sqf";				// Micovery vehicle actions
+	[] execVM "addons\APOC_Airdrop_Assistance\init.sqf";	// Airdrop
+	[] execVM "addons\AF_Keypad\AF_KP_vars.sqf";			// Keypad for base locking
+};